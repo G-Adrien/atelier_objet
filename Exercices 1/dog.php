6c337b3a110519e239ba4dbd56c277b3960e898f
Adrien Girin <girin.adrien.pro@gmail.com>
2020-10-13 11:39:48 +0200

    updata exo 1

--- Exercices 1/dog.php
+++ Exercices 1/dog.php
@@ -5,8 +5,11 @@ class Dog
 
 }
 
-//On vient d'instancer une classe avec pour nom "Dog"
-//On peut donner un nom différent à la classe mais toujours avec la première lettre en majuscule
-// Il existe 
+/*On vient d'instancer une classe avec pour nom "Dog"
+On peut donner un nom différent à la classe mais toujours avec la première
+lettre en majuscule
+Il existe deux méthodes pour appeler un fichier : include() et require() 
+en cas d'erreur include continue l'exécution du script.
+*/
 
 ?>
